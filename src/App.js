6c7bd8690891d6d1666676427f0ba409042b18be
geodocsservice-lab <geodocsservice@gmail.com>
2026-07-11 00:49:06 +0400

    Remove React import from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Home, LayoutGrid, Info, Instagram, Facebook, Send, X, FileText } from 'lucide-react';
 
 export default function GeoDocsApp() {
