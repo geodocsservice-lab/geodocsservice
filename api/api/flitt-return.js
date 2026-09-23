3aad41d8b13df4887f0bc29e3a2062962bfeab30
geodocsservice-lab <geodocsservice@gmail.com>
2026-09-24 00:24:01 +0400

    Delete api/api/flitt-return.js

--- api/api/flitt-return.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  // Flitt-დან მოსული POST მოთხოვნის დაჭერა და GET მეთოდით მთავარ გვერდზე გადამისამართება
-  res.redirect(302, '/?payment_status=success');
-}
